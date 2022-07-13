b85aa0c7f46e222adbd40adacfbc3b9e3cc69743
Pritam Shinde <pritamshinde.success@gmail.com>
2022-07-13 11:37:04 +0530

    last update 130720221137

--- src/Components/CommonPage/Data/Breadcrumb.jsx
+++ src/Components/CommonPage/Data/Breadcrumb.jsx
@@ -27,6 +27,40 @@ const Breadcrumb = [
         id:'Covid',
         slug:'/how-we-can-reduce-the-spread-of-corona-virus-together/',
         links: [{ id: "Covid-3.1", links: null, text: 'HOW WE CAN REDUCE THE SPREAD OF CORONA VIRUS TOGETHER'}]
+    },
+    {
+        id:'skye',
+        slug:'/get-painless-dental-treatment-at-skye/',
+        links: [{ id: "Covid-3.1", links: null, text: 'GET PAINLESS DENTAL TREATMENT AT SKYE'}]
+    },   {
+        id:'sandhurst',
+        slug:'/get-the-best-dental-care-at-sandhurst/',
+        links: [{ id: "Covid-3.1", links: null, text: 'GET THE BEST DENTAL CARE AT SANDHURST'}]
+    },  {
+        id:'patterson-lake',
+        slug:'/affordable-dental-services-at-patterson-lakes/',
+        links: [{ id: "Covid-3.1", links: null, text: 'AFFORDABLE DENTAL SERVICES AT PATTERSON LAKES'}]
+    }, {
+        id:'carbourne-west',
+        slug:'/we-are-your-go-to-dentist-in-cranbourne-west/',
+        links: [{ id: "Covid-3.1", links: null, text: 'WE ARE YOUR GO-TO DENTIST IN CRANBOURNE WEST'}]
+    },
+    {
+        id:'frankston-north',
+        slug:'/looking-for-a-dentist-in-frankston-north/',
+        links: [{ id: "Covid-3.1", links: null, text: 'LOOKING FOR A DENTIST IN FRANKSTON NORTH?'}]
+    },  {
+        id:'seaford',
+        slug:'/find-the-best-dentist-in-seaford/',
+        links: [{ id: "Covid-3.1", links: null, text: 'FIND THE BEST DENTIST IN SEAFORD'}]
+    }, {
+        id:'lyndhurst',
+        slug:'/get-all-your-dental-needs-handled-in-lyndhurst/',
+        links: [{ id: "Covid-3.1", links: null, text: 'GET ALL YOUR DENTAL NEEDS HANDLED IN LYNDHURST'}]
+    }, {
+        id:'langwarrin',
+        slug:'/affordable-dental-services-at-langwarrin/',
+        links: [{ id: "Covid-3.1", links: null, text: 'AFFORDABLE DENTAL SERVICES AT LANGWARRIN'}]
     }
 ];
 
